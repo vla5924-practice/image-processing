a0744371d66499824174b0abfd3a20968a7c8c1a
Alexandr-Solovev <n19ht1n9ale27@gmail.com>
2020-09-25 18:04:54 +0300

    added luminosity

--- 01_filters/grayscale.py
+++ 01_filters/grayscale.py
@@ -1,2 +1,19 @@
 import numpy
 import cv2
+
+
+def luminosity(bgr_image: numpy.ndarray):
+    width, height, _ = bgr_image.shape
+    gs_image = numpy.zeros((width, height), "ubyte")
+    for x in range(width):
+        for y in range(height):
+            gs_image[x, y] = 0.21 * bgr_image[x, y, 2] + 0.72 * bgr_image[x, y, 1] + 0.07 * bgr_image[x, y, 0]
+    return gs_image
+
+
+def test(filename):
+    image = cv2.imread(filename)
+    gs_image = luminosity(image)
+    cv2.imshow("Image", gs_image)
+    cv2.waitKey(0)
+    cv2.destroyAllWindows()
