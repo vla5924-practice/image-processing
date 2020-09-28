529650a3784de7cde2fb5d78d7e05d0a11bbc739
vla5924 <vla2000@inbox.ru>
2020-09-28 14:40:09 +0300

    color_models refactoring

--- 01_filters/color_models.py
+++ 01_filters/color_models.py
@@ -2,14 +2,39 @@ import numpy
 import cv2
 
 
+def calculate_h(r, g, b, c_min=None, c_max=None):
+    h = 0
+    c_min = min(r, g, b) if c_min is None else c_min
+    c_max = max(r, g, b) if c_max is None else c_max
+    delta = c_max - c_min
+    if delta == 0:
+        h = 0
+    elif c_max == r:
+        h = (60 * (g - b) / delta) % 6
+    elif c_max == g:
+        h = 60 * ((b - r) / delta + 2)
+    elif c_max == b:
+        h = 60 * ((r - g) / delta + 4)
+    return h
+
+
+def calculate_s(r, g, b, c_min=None, c_max=None):
+    c_min = min(r, g, b) if c_min is None else c_min
+    c_max = max(r, g, b) if c_max is None else c_max
+    delta = c_max - c_min
+    s = delta / c_max if c_max != 0 else 0
+    return s
+
+
 def bgr_to_hsv(image: numpy.ndarray):
-    width, height, _ = image.shape
-    r = numpy.zeros((width, height), "float")
-    g = numpy.zeros((width, height), "float")
-    b = numpy.zeros((width, height), "float")
-    c_max = numpy.zeros((width, height), "float")
-    c_min = numpy.zeros((width, height), "float")
-    delta = numpy.zeros((width, height), "float")
+    width, height, dim = image.shape
+    if dim != 3:
+        raise ValueError("Image must have 3 color channels")
+    r = numpy.zeros((width, height), "float32")
+    g = numpy.zeros((width, height), "float32")
+    b = numpy.zeros((width, height), "float32")
+    c_max = numpy.zeros((width, height), "float32")
+    c_min = numpy.zeros((width, height), "float32")
     for x in range(width):
         for y in range(height):
             r[x, y] = image[x, y, 2] / 255
@@ -17,38 +42,22 @@ def bgr_to_hsv(image: numpy.ndarray):
             b[x, y] = image[x, y, 0] / 255
             c_max[x, y] = max(r[x, y], g[x, y], b[x, y])
             c_min[x, y] = min(r[x, y], g[x, y], b[x, y])
-            delta[x, y] = c_max[x, y] - c_min[x, y]
-    h = numpy.zeros((width, height), "float")
-    s = numpy.zeros((width, height), "float")
-    v = numpy.zeros((width, height), "float")
+    hsv_image = numpy.zeros((width, height, 3), "float32")
     for x in range(width):
         for y in range(height):
-            if delta[x, y] == 0:
-                h[x, y] = 0
-            elif c_max[x, y] == r[x, y]:
-                h[x, y] = (60 * (g[x, y] - b[x, y]) / delta[x, y]) % 6
-            elif c_max[x, y] == g[x, y]:
-                h[x, y] = 60 * ((b[x, y] - r[x, y]) / delta[x, y] + 2)
-            elif c_max[x, y] == b[x, y]:
-                h[x, y] = 60 * ((r[x, y] - g[x, y]) / delta[x, y] + 4)
-            if c_max[x, y] == 0:
-                s[x, y] = 0
-            else:
-                s[x, y] = delta[x, y] / c_max[x, y]
-            v[x, y] = c_max[x, y]
-    hsv_image = numpy.zeros((width, height, 3), "float")
-    for x in range(width):
-        for y in range(height):
-            hsv_image[x, y, 0] = h[x, y]
-            hsv_image[x, y, 1] = s[x, y]
-            hsv_image[x, y, 2] = v[x, y]
+            hsv_image[x, y, 0] = calculate_h(r[x,y], g[x, y], b[x, y], c_min[x, y], c_max[x, y])
+            hsv_image[x, y, 1] = calculate_s(r[x,y], g[x, y], b[x, y], c_min[x, y], c_max[x, y])
+            hsv_image[x, y, 2] = c_max[x, y]
     return hsv_image
 
 
 def test(filename):
     image = cv2.imread(filename)
-    result = numpy.float32(bgr_to_hsv(image))
-    bgr = cv2.cvtColor(result, cv2.COLOR_HSV2BGR)
+    bgr = cv2.cvtColor(bgr_to_hsv(image), cv2.COLOR_HSV2BGR)
     cv2.imshow("Image", bgr)
     cv2.waitKey(0)
     cv2.destroyAllWindows()
+
+
+if __name__ == "__main__":
+    test("image.png")
