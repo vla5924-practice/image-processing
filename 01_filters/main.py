8f4d7de5463bffd118f9b3487f10412007126bce
Alexandr-Solovev <n19ht1n9ale27@gmail.com>
2020-10-08 19:05:22 +0300

    update main

--- 01_filters/main.py
+++ 01_filters/main.py
@@ -2,21 +2,35 @@ import cv2
 import metrics
 import grayscale
 import color_models
+import time
 
 
 bgr_image = cv2.imread("image.png")
 
-# 1. Metrics
-pass
 
-# 2. Grayscale
+# Grayscale
 gs_image = grayscale.luminosity(bgr_image)
 cv2.imwrite('gs_image.png', gs_image)
 gscv2_image = cv2.cvtColor(bgr_image, cv2.COLOR_BGR2GRAY)
-cv2.imwrite('gscv2_image.png', gs_image)
+cv2.imwrite('gscv2_image.png', gscv2_image)
 
-# 3. Color models
-hsv_image = color_models.bgr_to_hsv(bgr_image)
-cv2.imwrite('hsv_image.png', hsv_image)
-hsvcv2_image = cv2.cvtColor(bgr_image, cv2.COLOR_BGR2HSV)
-cv2.imwrite('hsvcv2_image.png', hsv_image)
+
+# Color models
+time_start1 = time.clock()
+image1 = color_models.bgr_to_hsv(bgr_image)
+image1 = color_models.hsv_to_bgr(image1)
+time_end1 = time.clock()
+cv2.imwrite('image1.png', image1)
+time_start2 = time.clock()
+image2 = cv2.cvtColor(bgr_image, cv2.COLOR_BGR2HSV)
+image2 = cv2.cvtColor(image2, cv2.COLOR_HSV2BGR)
+time_end2 = time.clock()
+cv2.imwrite('image2.png', image2)
+time_delta1 = time_end1 - time_start1
+time_delta2 = time_end2 - time_start2
+print(time_delta1, time_delta2)
+mse_image1 = metrics.mse(bgr_image, image1)
+mse_image2 = metrics.mse(bgr_image, image2)
+print(mse_image1, mse_image2)
+image3 = color_models.bgr_brighten(bgr_image, 1.05)
+cv2.imwrite('image3.png', image3)
