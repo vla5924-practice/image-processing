2ba5a82ec27e5842838b37c5df40793ebe6b1830
vla5924 <vla2000@inbox.ru>
2020-10-11 15:52:05 +0300

    added iterations count

--- 01_filters/main.py
+++ 01_filters/main.py
@@ -28,17 +28,20 @@ cv2.imwrite('hsvcv2_image.png', hsvcv2_image)
 convertedcv2_image = cv2.cvtColor(hsvcv2_image, cv2.COLOR_HSV2BGR)
 cv2.imwrite('convertedcv2_image.png', convertedcv2_image)
 
+iterations = 1
 time_start = time()
-image1 = color_models.bgr_brighten(bgr_image, 1.2)
-image2 = color_models.hsv_to_bgr(color_models.hsv_brighten(color_models.bgr_to_hsv(bgr_image), 1.2))
+for i in range(iterations):
+    image1 = color_models.bgr_brighten(bgr_image, 1.2)
+    image2 = color_models.hsv_to_bgr(color_models.hsv_brighten(color_models.bgr_to_hsv(bgr_image), 1.2))
 time1 = time() - time_start
 mse1 = metrics.mse(image1, image2)
 cv2.imwrite('bgr_brighten.png', image1)
 cv2.imwrite('converted_brighten.png', image2)
 
 time_start = time()
-image1 = color_models.bgr_brighten(bgr_image, 1.2)
-image2 = cv2.cvtColor(color_models.hsv_brighten(cv2.cvtColor(bgr_image, cv2.COLOR_BGR2HSV), 1.2), cv2.COLOR_HSV2BGR)
+for i in range(iterations):
+    image1 = color_models.bgr_brighten(bgr_image, 1.2)
+    image2 = cv2.cvtColor(color_models.hsv_brighten(cv2.cvtColor(bgr_image, cv2.COLOR_BGR2HSV), 1.2), cv2.COLOR_HSV2BGR)
 time2 = time() - time_start
 mse2 = metrics.mse(image1, image2)
 cv2.imwrite('convertedcv2_brighten.png', image2)
