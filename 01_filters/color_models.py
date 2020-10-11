0f46c5ebf2b456a5288b79117d0e723ed6179bd0
vla5924 <vla2000@inbox.ru>
2020-10-11 15:24:53 +0300

    color_models conversion algorithms rewrite

--- 01_filters/color_models.py
+++ 01_filters/color_models.py
@@ -2,111 +2,100 @@ import numpy
 import cv2
 
 
-def clamp(value, minimal, maximum):
+def clamp(value, minimal, maximal):
     if value < minimal:
         return minimal
-    if value > maximum:
-        return maximum
+    if value > maximal:
+        return maximal
     return value
 
 
-def calculate_h(r, g, b, c_min=None, c_max=None):
-    h = 0
-    c_min = min(r, g, b) if c_min is None else c_min
-    c_max = max(r, g, b) if c_max is None else c_max
-    delta = c_max - c_min
-    if delta == 0:
-        h = 0
-    elif c_max == r:
-        h = (60 * (g - b) / delta) % 6
-    elif c_max == g:
-        h = 60 * ((b - r) / delta + 2)
-    elif c_max == b:
-        h = 60 * ((r - g) / delta + 4)
-    return clamp(h, 0, 360)
-
-
-def calculate_s(r, g, b, c_min=None, c_max=None):
-    c_min = min(r, g, b) if c_min is None else c_min
-    c_max = max(r, g, b) if c_max is None else c_max
-    delta = c_max - c_min
-    s = delta / c_max if c_max != 0 else 0
-    return clamp(s, 0, 1)
-
-
-def bgr_to_hsv(image: numpy.ndarray):
+def bgr_to_hsv(image: numpy.ndarray) -> numpy.ndarray:
     width, height, dim = image.shape
     if dim != 3:
         raise ValueError("Image must have 3 color channels")
-    r = numpy.zeros((width, height), "float32")
-    g = numpy.zeros((width, height), "float32")
-    b = numpy.zeros((width, height), "float32")
-    c_max = numpy.zeros((width, height), "float32")
-    c_min = numpy.zeros((width, height), "float32")
+    hsv_image = numpy.zeros((width, height, 3), "int32")
+    coef = 360 / 255
     for x in range(width):
         for y in range(height):
-            r[x, y] = image[x, y, 2] / 255
-            g[x, y] = image[x, y, 1] / 255
-            b[x, y] = image[x, y, 0] / 255
-            c_max[x, y] = max(r[x, y], g[x, y], b[x, y])
-            c_min[x, y] = min(r[x, y], g[x, y], b[x, y])
-    hsv_image = numpy.zeros((width, height, 3), "float32")
-    for x in range(width):
-        for y in range(height):
-            hsv_image[x, y, 0] = calculate_h(r[x, y], g[x, y], b[x, y], c_min[x, y], c_max[x, y])
-            hsv_image[x, y, 1] = calculate_s(r[x, y], g[x, y], b[x, y], c_min[x, y], c_max[x, y])
-            hsv_image[x, y, 2] = clamp(c_max[x, y], 0, 1)
+            r = image[x, y, 2]
+            g = image[x, y, 1]
+            b = image[x, y, 0]
+            c_max = max(r, g, b)
+            c_min = min(r, g, b)
+            delta = c_max - c_min
+            h = 0
+            s = 0
+            v = c_max
+            if v != 0:
+                s = 255 * delta / v
+                if s != 0:
+                    if c_max == r:
+                        h = 43 * (g - b) / delta + 0
+                    elif c_max == g:
+                        h = 43 * (b - r) / delta + 85
+                    else:
+                        safe_r_minus_g = r - g if r > g else 0
+                        h = 43 * safe_r_minus_g / delta + 171
+            hsv_image[x, y, 0] = clamp(h / coef, 0, 255)
+            hsv_image[x, y, 1] = clamp(s, 0, 255)
+            hsv_image[x, y, 2] = clamp(v, 0, 255)
     return hsv_image
 
 
-def hsv_to_bgr(image: numpy.ndarray):
+def hsv_to_bgr(image: numpy.ndarray) -> numpy.ndarray:
+    """
+    Formula from Wikipedia https://en.wikipedia.org/wiki/HSV_color_space
+    """
     width, height, dim = image.shape
     if dim != 3:
-        raise ValueError("Image must have 3 colors channel")
-    r = numpy.zeros((width, height), "float32")
-    g = numpy.zeros((width, height), "float32")
-    b = numpy.zeros((width, height), "float32")
-    m = numpy.zeros((width, height), "float32")
+        raise ValueError("Image must have 3 color channels")
+    bgr_image = numpy.zeros((width, height, 3), "int32")
+    coef = 360 / 255
     for x in range(width):
         for y in range(height):
-            c = image[x, y, 2] * image[x, y, 1]
-            d = c * (1 - abs((image[x, y, 0] / 60) % 2 - 1))
-            m[x, y] = image[x, y, 2] - c
-            h = image[x, y, 0]
-            if 0 <= h < 60:
-                r[x, y] = c
-                g[x, y] = d
-                b[x, y] = 0
-            elif 60 <= h < 120:
-                r[x, y] = d
-                g[x, y] = c
-                b[x, y] = 0
-            elif 120 <= h < 180:
-                r[x, y] = 0
-                g[x, y] = c
-                b[x, y] = d
-            elif 180 <= h < 240:
-                r[x, y] = 0
-                g[x, y] = d
-                b[x, y] = c
-            elif 240 <= h < 300:
-                r[x, y] = d
-                g[x, y] = 0
-                b[x, y] = c
-            elif 300 <= h < 360:
-                r[x, y] = c
-                g[x, y] = 0
-                b[x, y] = d
-    bgr_image = numpy.zeros((width, height, 3), "float32")
-    for x in range(width):
-        for y in range(height):
-            bgr_image[x, y, 0] = clamp((b[x, y] + m[x, y]) * 255, 0, 255)
-            bgr_image[x, y, 1] = clamp((g[x, y] + m[x, y]) * 255, 0, 255)
-            bgr_image[x, y, 2] = clamp((r[x, y] + m[x, y]) * 255, 0, 255)
+            h = image[x, y, 0] * coef
+            s = image[x, y, 1] / 2.55
+            v = image[x, y, 2] / 2.55
+            h_i = numpy.floor(h / 60) % 6
+            v_min = (100 - s) * v / 100
+            a = (v - v_min) * ((h % 60) / 60)
+            v_inc = v_min + a
+            v_dec = v - a
+            r = 0
+            g = 0
+            b = 0
+            if h_i == 0:
+                r = v
+                g = v_inc
+                b = v_min
+            elif h_i == 1:
+                r = v_dec
+                g = v
+                b = v_min
+            elif h_i == 2:
+                r = v_min
+                g = v
+                b = v_inc
+            elif h_i == 3:
+                r = v_min
+                g = v_dec
+                b = v
+            elif h_i == 4:
+                r = v_min
+                g = v_inc
+                b = v
+            elif h_i == 5:
+                r = v
+                g = v_min
+                b = v_dec
+            bgr_image[x, y, 0] = clamp(b * 2.55, 0, 255)
+            bgr_image[x, y, 1] = clamp(g * 2.55, 0, 255)
+            bgr_image[x, y, 2] = clamp(r * 2.55, 0, 255)
     return bgr_image
 
 
-def bgr_brighten(image: numpy.ndarray, bright_coef):
+def bgr_brighten(image: numpy.ndarray, bright_coef) -> numpy.ndarray:
     width, height, dim = image.shape
     if dim != 3:
         raise ValueError("Image must have 3 colors channel")
@@ -116,15 +105,15 @@ def bgr_brighten(image: numpy.ndarray, bright_coef):
             bgr_image[x, y, 0] = clamp(image[x, y, 0] * bright_coef, 0, 255)
             bgr_image[x, y, 1] = clamp(image[x, y, 1] * bright_coef, 0, 255)
             bgr_image[x, y, 2] = clamp(image[x, y, 2] * bright_coef, 0, 255)
-    return bgr_image
+    return bgr_image.astype("int32")
 
 
-def hsv_brighten(image: numpy.ndarray, bright_coef):
+def hsv_brighten(image: numpy.ndarray, bright_coef) -> numpy.ndarray:
     width, height, dim = image.shape
     if dim != 3:
         raise ValueError("Image must have 3 colors channel")
     hsv_image = image.copy()
     for x in range(width):
         for y in range(height):
-            hsv_image[x, y, 2] = clamp(hsv_image[x, y, 2] * bright_coef, 0, 1)
+            hsv_image[x, y, 2] = clamp(image[x, y, 2] * bright_coef, 0, 255)
     return hsv_image
