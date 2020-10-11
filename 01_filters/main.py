91df5b29ffad188c101d02c83736741aedb56481
vla5924 <vla2000@inbox.ru>
2020-10-11 15:27:16 +0300

    added live tests and time tracking

--- 01_filters/main.py
+++ 01_filters/main.py
@@ -1,36 +1,47 @@
 import cv2
+from time import time
 import metrics
 import grayscale
 import color_models
-import time
 
 
 bgr_image = cv2.imread("image.png")
 
-
 # Grayscale
 gs_image = grayscale.luminosity(bgr_image)
 cv2.imwrite('gs_image.png', gs_image)
+
 gscv2_image = cv2.cvtColor(bgr_image, cv2.COLOR_BGR2GRAY)
 cv2.imwrite('gscv2_image.png', gscv2_image)
 
 
 # Color models
-time_start1 = time.clock()
-image1 = color_models.bgr_to_hsv(bgr_image)
-image1 = color_models.hsv_to_bgr(image1)
-time_end1 = time.clock()
-cv2.imwrite('image1.png', image1)
-time_start2 = time.clock()
-image2 = cv2.cvtColor(bgr_image, cv2.COLOR_BGR2HSV)
-image2 = cv2.cvtColor(image2, cv2.COLOR_HSV2BGR)
-time_end2 = time.clock()
-cv2.imwrite('image2.png', image2)
-time_delta1 = time_end1 - time_start1
-time_delta2 = time_end2 - time_start2
-print(time_delta1, time_delta2)
-mse_image1 = metrics.mse(bgr_image, image1)
-mse_image2 = metrics.mse(bgr_image, image2)
-print(mse_image1, mse_image2)
-image3 = color_models.bgr_brighten(bgr_image, 1.05)
-cv2.imwrite('image3.png', image3)
+hsv_image = color_models.bgr_to_hsv(bgr_image)
+cv2.imwrite('hsv_image.png', hsv_image)
+
+converted_image = color_models.hsv_to_bgr(hsv_image)
+cv2.imwrite('converted_image.png', converted_image)
+
+hsvcv2_image = cv2.cvtColor(bgr_image, cv2.COLOR_BGR2HSV)
+cv2.imwrite('hsvcv2_image.png', hsvcv2_image)
+
+convertedcv2_image = cv2.cvtColor(hsvcv2_image, cv2.COLOR_HSV2BGR)
+cv2.imwrite('convertedcv2_image.png', convertedcv2_image)
+
+time_start = time()
+image1 = color_models.bgr_brighten(bgr_image, 1.2)
+image2 = color_models.hsv_to_bgr(color_models.hsv_brighten(color_models.bgr_to_hsv(bgr_image), 1.2))
+time1 = time() - time_start
+mse1 = metrics.mse(image1, image2)
+cv2.imwrite('bgr_brighten.png', image1)
+cv2.imwrite('converted_brighten.png', image2)
+
+time_start = time()
+image1 = color_models.bgr_brighten(bgr_image, 1.2)
+image2 = cv2.cvtColor(color_models.hsv_brighten(cv2.cvtColor(bgr_image, cv2.COLOR_BGR2HSV), 1.2), cv2.COLOR_HSV2BGR)
+time2 = time() - time_start
+mse2 = metrics.mse(image1, image2)
+cv2.imwrite('convertedcv2_brighten.png', image2)
+
+print(time1, time2)
+print(mse1, mse2)
