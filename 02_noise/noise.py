31cac3b0db6ca4abbb9623472e62e2a51748fec6
Alexandr-Solovev <n19ht1n9ale27@gmail.com>
2020-11-01 12:41:38 +0300

    add salt_and_pepper

--- 02_noise/noise.py
+++ 02_noise/noise.py
@@ -1,6 +1,17 @@
 import numpy
-from clamp import clamp
 
 
 def salt_and_pepper(gs_image: numpy.ndarray) -> numpy.ndarray:
-    raise ValueError("Hello")
+    if gs_image.ndim != 2:
+        raise ValueError("Image must have 1 color channel")
+    width, height = gs_image.shape
+    salt_pepper_noise = numpy.random.uniform(0, 255, (width, height))
+    result_image = gs_image.copy()
+    for x in range(width):
+        for y in range(height):
+            if salt_pepper_noise[x, y] < 30:
+                result_image[x, y] = 0
+            elif salt_pepper_noise[x, y] > 225:
+                result_image[x, y] = 255
+    return result_image
+
