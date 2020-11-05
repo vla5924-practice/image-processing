ee662020fc81be3cf908965c637dd746c5096a80
vla5924 <vla5924@gmail.com>
2020-11-05 18:28:04 +0300

    added time measurement for averaging

--- 02_noise/main.py
+++ 02_noise/main.py
@@ -9,20 +9,21 @@ gs_image = cv2.cvtColor(gs_image, cv2.COLOR_BGR2GRAY)
 noise_image = noise.salt_and_pepper(gs_image)
 cv2.imwrite('noise_image.png', noise_image)
 
+time_start = time()
 avg_image = filters.averaging(noise_image, 1)
+time1 = time() - time_start
 cv2.imwrite('avg_image.png', avg_image)
-
 
 time_start = time()
 median_image = filters.median(noise_image, 1)
-time1 = time() - time_start
+time2 = time() - time_start
 cv2.imwrite('median_image.png', median_image)
 
 time_start = time()
 mediancv2_image = noise_image.copy()
 cv2.medianBlur(noise_image, 3, mediancv2_image)
-time2 = time() - time_start
+time3 = time() - time_start
 cv2.imwrite('mediancv2_image.png', mediancv2_image)
 
-print(time1, time2)
+print("averaging:", time1, "median:", time2, "mediancv2:", time3)
 
